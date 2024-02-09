b3b992e5b2e1651974813b0be1aff902d03e62bb
munashex <munashemugondaa@gmail.com>
2024-02-09 10:21:57 +0200

    expect methods

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ const App = () => {
   return (
     <div>
       <button>Click me</button>
+      <h1>munashe</h1> 
+      <a href="muna">munashe</a>
+      <input type="checkbox" checked/>
+      <input type="text" value="ghosts" name="search movie"/>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,22 @@ test('testing button', () => {
     const button = screen.getByRole('button', {name: 'Click me'}) 
     expect(button).toBeInTheDocument()
 })
+
+test("check munashe text", () => {
+    render(<App/>) 
+    const h1text = screen.getByRole('heading') 
+    expect(h1text).toHaveTextContent('munashe')
+})
+
+test("testing link", () => {
+    render(<App/>) 
+    const linktext = screen.getByRole('link', {name: 'munashe'}) 
+    expect(linktext).toHaveAttribute('href', 'muna')
+})
+
+test("testing checkbox", () => {
+    render(<App/>) 
+    const checkbox = screen.getByRole('checkbox') 
+    expect(checkbox).toBeChecked()
+})
+
